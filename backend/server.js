2019f56f026f8d2ce17db30ac26178cded54f6c1
vinayak501 <vinayakchavda2004@gmail.com>
2024-01-17 17:59:15 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 const userRoutes = require('./routes/userRoutes');
 const noteRoutes = require('./routes/noteRoutes');
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const path = require('path');
 
 
 
@@ -13,12 +14,25 @@ dotenv.config();
 connectDB();
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("Working");
-})
 
 app.use('/api/users',userRoutes);
 app.use('/api/notes',noteRoutes);
+
+// ----------------------------DEPLOYMENT--------------------------------------------------------------
+
+const __dirname1 = path.resolve();
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname1,'/frontend/build')));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"));
+    })
+}else{
+    app.get("/",(req,res)=>{
+        res.send("Working");
+    })
+}
+
+// ----------------------------DEPLOYMENT--------------------------------------------------------------
 
 app.use(notFound);
 app.use(errorHandler);
